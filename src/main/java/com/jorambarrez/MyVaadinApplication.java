1ad7cb5d5f17398786e24a607308e6deb723fbdc
Joram Barrez <joram.barrez@gmail.com>
2011-04-26 22:40:26 +0200

    Got Refresher component working, but slow build is unacceptable to be a valid approach

---
+++ src/main/java/com/jorambarrez/MyVaadinApplication.java
@@ -0,0 +1,65 @@
+package com.jorambarrez;
+
+import com.github.wolfie.refresher.Refresher;
+import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class MyVaadinApplication extends Application {
+
+  private Window window;
+  private Label label;
+  private Refresher refresher;
+  private Button countButton;
+
+  @Override
+  public void init() {
+    
+    window = new Window("My Vaadin Application");
+    setMainWindow(window);
+
+    label = new Label("ping");
+    window.addComponent(label);
+    
+    refresher = new Refresher();
+    window.addComponent(refresher);
+    
+    countButton = new Button("Start");
+    window.addComponent(countButton);
+    countButton.addListener(new ClickListener() {
+      public void buttonClick(ClickEvent event) {
+        refresher.setRefreshInterval(2000L);
+        Thread thread = new Thread(new PingPongRunnable());
+        thread.start();
+      }
+    });
+  }
+  
+  class PingPongRunnable implements Runnable {
+    
+    public void run() {
+      while (true) {
+        synchronized (MyVaadinApplication.this) {
+          if (label.getValue().equals("ping")) {
+            label.setValue("pong");
+          } else {
+            label.setValue("ping");
+          }
+        }
+        
+        try {
+          Thread.sleep(500L);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      
+    }
+    
+  }
+
+}
